ddad9c0859be8982b6a2fe19c8c8a8b40674bd48
anonymotter <kstefun@csumb.edu>
2023-12-02 17:59:48 -0800

    Start on login functionality

---
+++ app/src/main/java/com/example/cnc/DB/CncDao.java
@@ -0,0 +1,39 @@
+package com.example.cnc.DB;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.cnc.User;
+
+import java.util.List;
+
+/**
+ * @author Kyle Stefun
+ * @since 2023.12.01
+ * Data access object for the C&C application.
+ */
+
+@Dao
+public interface CncDao {
+
+  @Insert
+  void insert(User... users);
+
+  @Update
+  void upgrade(User... users);
+
+  @Delete
+  void delete(User user);
+
+  @Query("SELECT * FROM " + AppDatabase.USER_TABLE)
+  List<User> getAllUsers();
+
+  @Query("SELECT * FROM " + AppDatabase.USER_TABLE + " WHERE username = :username")
+  List<User> getUserByName(String username);
+
+  @Query("SELECT * FROM " + AppDatabase.USER_TABLE + " WHERE username = :username AND password = :password")
+  List<User> getUserByNameAndPassword(String username, String password);
+}
---
+++ app/src/main/java/com/example/cnc/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.cnc;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.cnc.DB.AppDatabase;
+import com.example.cnc.DB.CncDao;
+import com.example.cnc.databinding.ActivityMainBinding;
+
+/**
+ * @author Kyle Stefun
+ * @since 2023.12.01
+ * First activity that a user sees, prompting them to log in or create an account.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+  private EditText usernameEdit;
+  private EditText passwordEdit;
+  private Button loginButton;
+  private Button createUserButton;
+
+  private ActivityMainBinding bind;
+
+  private CncDao cncDao;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    bind = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(bind.getRoot());
+    usernameEdit = bind.usernameEdit;
+    passwordEdit = bind.passwordEdit;
+    loginButton = bind.loginButton;
+    createUserButton = bind.createUserButton;
+
+    cncDao = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.DATABASE_NAME)
+        .allowMainThreadQueries().build().CnCDao();
+
+    loginButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        login();
+      }
+    });
+
+    createUserButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        createUser();
+      }
+    });
+  }
+
+  private void login() {
+    String username = usernameEdit.getText().toString();
+    String password = passwordEdit.getText().toString();
+    if(fieldsAreEmpty(username, password)) return;
+    if (cncDao.getUserByNameAndPassword(username, password).size() > 0) {
+      loginButton.setText("success");
+    } else {
+      loginButton.setText("failure");
+    }
+  }
+
+  private void createUser() {
+    String username = usernameEdit.getText().toString();
+    String password = passwordEdit.getText().toString();
+    if(fieldsAreEmpty(username, password)) return;
+    if (cncDao.getUserByName(username).size() > 0) {
+      loginButton.setText("User already exists");
+    } else {
+      cncDao.insert(new User(username, password, false));
+      loginButton.setText("User created");
+    }
+  }
+
+  private boolean fieldsAreEmpty(String username, String password) {
+    if (username.isEmpty()) {
+      loginButton.setText("Enter name");
+      return true;
+    } else if (password.isEmpty()) {
+      loginButton.setText("Enter password");
+      return true;
+    }
+    return false;
+  }
+}
