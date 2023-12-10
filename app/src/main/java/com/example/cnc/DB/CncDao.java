f528b50eb3f1db0ffc408cf9243d3568d57437fb
anonymotter <kstefun@csumb.edu>
2023-12-09 19:44:56 -0800

    Changing folder structure

--- app/src/main/java/com/example/cnc/DB/CncDao.java
+++ app/src/main/java/com/example/cnc/DB/CncDao.java
@@ -28,16 +28,16 @@ public interface CncDao {
   @Delete
   void delete(User user);
 
-  @Query("SELECT * FROM " + AppDatabase.USER_TABLE)
+  @Query("SELECT * FROM " + CncDb.USER_TABLE)
   List<User> getAllUsers();
 
-  @Query("SELECT * FROM " + AppDatabase.USER_TABLE + " WHERE username = :username")
+  @Query("SELECT * FROM " + CncDb.USER_TABLE + " WHERE username = :username")
   List<User> getUserByName(String username);
 
-  @Query("SELECT * FROM " + AppDatabase.USER_TABLE + " WHERE username = :username AND password = :password")
+  @Query("SELECT * FROM " + CncDb.USER_TABLE + " WHERE username = :username AND password = :password")
   List<User> getUserByNameAndPassword(String username, String password);
 
-  @Query("SELECT * FROM " + AppDatabase.USER_TABLE + " WHERE userId = :userId AND isDm = 1")
+  @Query("SELECT * FROM " + CncDb.USER_TABLE + " WHERE userId = :userId AND isDm = 1")
   List<User> getDmById(int userId);
 
 }
---
+++ app/src/main/java/com/example/cnc/DB/CncDb.java
@@ -0,0 +1,38 @@
+package com.example.cnc.DB;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import com.example.cnc.User;
+
+/**
+ * @author Kyle Stefun
+ * @since 2023.12.01
+ * Database class for the C&C application.
+ */
+
+@Database(entities = {User.class}, version = 1)
+public abstract class CncDb extends RoomDatabase {
+  public static final String DATABASE_NAME = "CnC.db";
+  public static final String USER_TABLE = "users_table";
+
+  private static volatile CncDb instance;
+
+  private static final Object LOCK = new Object();
+
+  public abstract CncDao CnCDao();
+
+  public static CncDb getInstance(Context context) {
+    if (instance == null) {
+      synchronized (LOCK) {
+        if (instance == null) {
+          instance = Room.databaseBuilder(context.getApplicationContext(),
+              CncDb.class,
+              DATABASE_NAME).build();
+        }
+      }
+    }
+    return instance;
+  }
+}
